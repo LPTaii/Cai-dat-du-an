947bc734aaab49231be3e8005c45c2fd0df7f27b
TaiLy <taib1900258@student.ctu.edu.vn>
2023-10-07 04:28:42 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-
-const contactsRouter = require("./app/routes/contact.route");
-const app = express();
-
 const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route");
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
-});
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res, next) => {
   // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
-  return next(new ApiError(404, "Resource not found"));
+  const err = new ApiError(404, "Not found");
+
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware lý lỗi
+  return next(err);
 });
 // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
   // Middleware xử lý lỗi tập trung.
   // Trong các đoạn code xử lý ở các route, gọi next(error)
   // sẽ chuyển về middleware xử lý lỗi này
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
   });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
 });
 module.exports = app;
